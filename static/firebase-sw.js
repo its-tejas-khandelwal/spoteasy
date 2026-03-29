8af0f3584026ada879a1d8a1272a2c669329c0db
its-tejas-khandelwal <khandelwaltejas2004@gmail.com>
2026-03-29 14:17:27 +0530

    fix: clean firebase-sw.js

--- static/firebase-sw.js
+++ static/firebase-sw.js
@@ -1,3 +1,1 @@
-self.addEventListener('install',function(){self.skipWaiting();});
-self.addEventListener('activate',function(e){e.waitUntil(clients.claim());});
-self.addEventListener('push',function(e){e.waitUntil(self.registration.showNotification('SpotEasy',{icon:'/static/icons/icon-192.png'}));});
+self.addEventListener('install',function(){self.skipWaiting();}); 
